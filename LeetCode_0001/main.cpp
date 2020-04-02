3d4d0acb603675f056abbe4bf2125677373e6c79
zuokunpeng <zuokunpengccsu@gmail.com>
2020-04-02 16:34:38 +0800

    两数之和

---
+++ LeetCode_0001/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2019年4月27日
+ *      Author: Zuokp
+ */
+#include "Solution.h"
+
+/*两数之和*/
+
+int main(int argc, char *argv[])
+{
+	int szData[] = {2, 7, 11, 15};
+
+	const int target = 9;
+	size_t size = sizeof(szData) / sizeof(szData[0]);
+	std::vector<int> vecNum(szData, szData + size);
+	std::vector<int> vecRet;
+
+	Solution sn;
+	vecRet = sn.twoSum(vecNum, target);
+
+	std::cout << "[" << vecRet[0] << ", " << vecRet[1] << "] " << std::endl;
+	return 0;
+}
+
